152ee67e62ed86e85c06978003d6f2446dc9a9be
Aimbotv2 <93753660+Aimbotv2@users.noreply.github.com>
2021-11-07 18:48:47 +0700

    Add files via upload

---
+++ butterC.py
@@ -0,0 +1,8 @@
+import os , sys , time
+os.system("clear")
+po = input("Enter the hack > ")
+time.sleep(3)
+os.system("clear")
+while 1==1:
+	time.sleep(0.03)
+	print("\033[1;95m Hacker\033[1;93m Hacker\033[1;96m Hacker\033[1;91m Hacker\033[1;96m Hacker\033[1;93m Hacker\033[1;96m Hacker")
---
+++ start.py
@@ -0,0 +1,28 @@
+import time , sys , os
+os.system("clear")
+print("\033[1;96m===Menu Enter===")
+print("")
+print("\033[1;95m[1] \033[1;92mยิงเบอร์")
+print("\033[1;95m[2] \033[1;92mยิงเบอร์ VIP")
+print("\033[1;95m[3] \033[1;92mยิงอีเมล์")
+print("\033[1;95m[4] \033[1;92mแฮกเกอร์")
+print("\033[1;95m[00] \033[1;92mออก")
+print("")
+print("\033[1;91m[*] By: Aimbot Sorecode Time: 7/11/2021")
+print("")
+verfly = input("\033[90m> \033[1;97mEnter the attack : \033[1;96m")
+if verfly == '1':
+	os.system("python Butter.py")
+	
+if verfly == '2':
+	os.system("python ButterA.py")
+	
+if verfly == '3':
+	os.system("exit")
+	
+if verfly == '4':
+	os.system("python ButterC.py")
+	
+if verfly == '00':
+	print("\033[1;96mAimbot")
+	os.system("exit")
